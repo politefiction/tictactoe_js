575c3817a1015d7d8ff391e515911f6f46184267
Chelsi Jones <cajones.bz@gmail.com>
2018-10-28 09:19:15 -0500

    Create functions for marking board

--- tictactoe.js
+++ tictactoe.js
@@ -1,11 +1,37 @@
 const squares = document.querySelectorAll(".square");
 
-const gameboard = (() => {
-    const addMarker = (square, marker) => square.textContent = marker;
+const Gameboard = (() => {
+    const addMarker = (square, marker) => {
+        if (square.textContent === "") {
+            square.textContent = marker;
+        }
+    }
     // check for victory?
     return { addMarker }
 })();
 
-const player = (name, marker) => {
-    return { name, marker }
+const Player = (name, marker) => {
+    const chooseSquare = (square) => {
+        Gameboard.addMarker(square, marker)
+    }
+    return { name, marker, chooseSquare }
 }
+
+const p1 = Player("Jane", "X");
+const p2 = Player("Chris", "O");
+let currentPlayer = p1;
+
+
+const GameControl = (() => {
+    const runGame = () => {
+        squares.forEach((square) => {
+            square.onclick = () => {
+                currentPlayer.chooseSquare(square);
+                currentPlayer = (currentPlayer == p1 ? p2 : p1);
+            }
+        })
+    }
+    return { runGame }
+})();
+
+GameControl.runGame();
